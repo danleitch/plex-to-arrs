effe4f0b58b5e4cc401b9ddc2ca0343ff2328c57
Daniel Leitch <40886941+danleitch@users.noreply.github.com>
2023-06-28 19:29:20 +0100

    first push

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# plex-to-arrs
+# Plex to Radarr Integration
+
+This repository provides a script for integrating Plex watchlist with Radarr. It allows users to automatically add movies from their Plex watchlist to Radarr for automated downloading and management.
+
+## Features
+
+- Fetch Plex watchlist and process movies
+- Retrieve TMDB ID for each movie
+- Add movies to Radarr using specified quality profile
+- Error handling for failed requests
+
+## Prerequisites
+
+Before using this script, make sure you have the following:
+
+- Python 3 installed on your system.
+- Radarr installed and configured with an API key.
+- Plex server with a valid Plex token.
+
+## Setup Instructions
+
+1. Clone the repository to your local machine or server.
+2. Install the required dependencies by running the following command:
+   ```
+   pip install -r requirements.txt
+   ```
+3. Open the `plex_to_arr.py` file in a text editor.
+4. Update the following variables with your own values:
+   - `PLEX_TOKEN`: Your Plex token.
+   - `RADARR_API_KEY`: Your Radarr API key.
+   - `RADARR_URL`: The URL of your Radarr instance.
+   - `RADARR_ROOT_FOLDER`: The root folder path where Radarr should save the downloaded movies.
+5. Save the changes.
+
+## Usage
+
+To use the script, follow these steps:
+
+1. Make sure Radarr and Plex are running and accessible.
+2. Open a terminal or command prompt.
+3. Navigate to the directory where the `plex_to_arr.py` file is located.
+4. Run the following command to start the script:
+   ```
+   python plex_to_arr.py
+   ```
+5. The script will fetch your Plex watchlist and process the movies.
+6. For each movie, it will search for the corresponding TMDB ID.
+7. If found, the movie will be added to Radarr using the specified quality profile.
+8. Check the script output for any errors or status updates.
+
+## Customization
+
+Feel free to customize the script further based on your specific setup and requirements. You can modify the quality profile, add additional error handling, or enhance the functionality as needed.
+
+## Contributions
+
+Contributions to this repository are welcome! If you have any improvements or bug fixes, feel free to submit a pull request.
+
+## Acknowledgements
+
+- The developers and contributors of Radarr, Plex, and TMDB for their fantastic tools and APIs.
---
+++ plex_to_arr.py
@@ -0,0 +1,99 @@
+import requests
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+
+PLEX_TOKEN = ""
+RADARR_API_KEY = ""
+RADARR_URL = ""
+RADARR_ROOT_FOLDER = ""
+
+def get_quality_profile_id():
+    radarr_url = "https://movies.niftytv.xyz/api/v3"
+    quality_profiles_url = f"{radarr_url}/qualityProfile?apikey={RADARR_API_KEY}"
+    response = requests.get(quality_profiles_url)
+    if response.status_code == 200:
+        quality_profiles = response.json()
+        for profile in quality_profiles:
+            if profile["name"] == "HD-1080p":
+                return profile["id"]
+    else:
+        print(f"Failed to retrieve quality profiles. Status Code: {response.status_code}")
+    return None
+
+QUALITY_PROFILE = get_quality_profile_id()
+
+
+def fetch_plex_watchlist():
+    print("Fetching Plex watchlist...")
+    plex_url = f"https://metadata.provider.plex.tv/library/sections/watchlist/all?X-Plex-Token={PLEX_TOKEN}"
+    response = requests.get(plex_url)
+    root = ET.fromstring(response.content)
+    return root.findall('Video')
+
+
+def fetch_tmdb_id(title):
+    print("Fetching TMDB ID...")
+    TMDB_API_KEY = "614ea6754d1fa006b1f8957fb9b5b3de"
+    TMDB_SEARCH_URL = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&query={title}"
+    response = requests.get(TMDB_SEARCH_URL)
+    if response.status_code == 200:
+        results = response.json().get('results')
+        if results:
+            # Assuming the first result is the most relevant one
+            return results[0]['id']
+        else:
+            print(f"No TMDB id found for {title}")
+            return None
+    else:
+        print(f"Failed to retrieve TMDB ID for {title}")
+        return None
+
+
+def add_to_radarr(tmdb_id, title):
+    print(f"Adding {title} to Radarr...")
+    payload = {
+        "title": title,
+        "qualityProfileId": int(QUALITY_PROFILE),
+        "tmdbId": tmdb_id,
+        "rootFolderPath": RADARR_ROOT_FOLDER,
+        "monitored": True,
+        "addOptions": {
+            "searchForMovie": True
+        }
+    }
+    radarr_add_url = f"{RADARR_URL}/movie?apikey={RADARR_API_KEY}"
+    print("Request Payload:")
+    print(payload)
+    response = requests.post(radarr_add_url, json=payload)
+    if response.status_code == 201:
+        print(f"Added {title} to Radarr successfully.")
+    else:
+        try:
+            error_message = response.json()[0]['errorMessage']
+            print(f"Failed to add {title} to Radarr. Error: {error_message}")
+        except (KeyError, IndexError):
+            print(f"Failed to add {title} to Radarr. Status Code: {response.status_code}")
+    print("Response Content:")
+    print(response.content.decode())
+
+
+
+
+def main():
+    print("Starting script...")
+    watchlist = fetch_plex_watchlist()
+    print(f"Found {len(watchlist)} items in Plex watchlist")
+    print("Processing Plex watchlist...")
+    for video in watchlist:
+        title = video.get('title')
+        publicPagesURL = video.get('publicPagesURL')
+        if publicPagesURL:
+            tmdb_id = fetch_tmdb_id(title)
+            if tmdb_id is not None:
+                add_to_radarr(tmdb_id, title)
+        else:
+            print(f"No 'publicPagesURL' found for {title}")
+
+
+if __name__ == "__main__":
+    main()
